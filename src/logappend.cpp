1daa8444733aab2eea417494a9cfcad84ea6ec35
Elliott Hager <ehager2@kent.edu>
2024-11-04 21:12:06 -0500

    Added in some more regex checking of arguments, have to figure out how to actually get the arguments that can move around dealt with (-G or -E, -R, and -A or -L)

--- src/logappend.cpp
+++ src/logappend.cpp
@@ -1,75 +1,156 @@
 #include <iostream>
 #include <regex>
+#include <string>
+#include <map>
 
 
-/*
-Command syntax
 
-logappend -T <timestamp> -K <token> (-E <employee-name> | -G <guest-name>) (-A | -L) [-R <room-id>] <log>
+using std::cout; using std::endl; using std::map; using std::string; using std::regex; using std::regex_match;
 
+bool sanatizeInput(int argc, char* argv[], map<string, string>& resultingMap) {
+    cout << ("Sanatizing the input!!!!") << endl;
 
-logappend -B <file>
+    ///Check for the correct argument count again
+    if (argc != 9 && argc != 11) {
+        return false;
+    }
 
-*/
+    //Now make the regex matchers
+    
+    //TimeStamp
+    regex timeStampDashMatcher("-T", std::regex_constants::ECMAScript);
+    regex timeStampMatcher (".*", std::regex_constants::ECMAScript);
 
-using std::cout; using std::endl; using std::regex; using std::regex_match;
-
-void sanatize(int argumentCount, char* arguments[]) {
-    //char* sanatizedOutput[];
-    //Make the regex matcher
-    regex timeStampDash("-T",  std::regex_constants::ECMAScript);
-
-    //Time stamp check
-    //Check if it matches
-    if (regex_match(arguments[1], timeStampDash)) {
-        cout << "-T MATCH!" << endl;
+    //Run the matcher and check
+    if(regex_match(argv[1], timeStampDashMatcher)) {
+        //Now we have a valid time flag
+        //Now check the time stamp
+        if (regex_match(argv[2], timeStampMatcher)) {
+            //Now sanitized and safe, now put in the map
+            resultingMap["T"] = string(argv[2]);
+        } else {
+            cout << "Timestamp was invalid!" << endl;
+            //The timestamp did not match format
+            return false;
+        }
+    } else {
+        cout << "Timestamp flag was not found!" << endl;
+        //The -T is missing
+        return false;
     }
 
 
+    //Token
+    regex tokenDashMatcher("-K", std::regex_constants::ECMAScript);
+    regex tokenMatcher("([a-z]|[A-Z]|[0-9])*", std::regex_constants::ECMAScript);
+
+    if (regex_match(argv[3], tokenDashMatcher)) {
+        //Now we have a valid token flag
+        //Now check the token
+        if(regex_match( argv[4], tokenMatcher)) {
+            //Now sanitized and safe, now put in the map
+            resultingMap["K"] = string(argv[4]);
+        } else {
+            // Token was not valid
+            cout << "Token was not valid!" << endl;
+            cout << "Token: |" << argv[4] << "|" << endl;
+            return false;
+        }
+    } else {
+        cout << "Token flag was not present!" << endl;
+        //Token flag was not found
+        return false;
+    }
+
+    //Guest OR Employee
+    regex guestNameDashMatcher("-G", std::regex_constants::ECMAScript);
+    regex employeeNameDashMatcher("-E", std::regex_constants::ECMAScript);
+
+    regex guestEmployeeNameMatcher("([a-z]|[A-Z])*", std::regex_constants::ECMAScript);
+
+    if (regex_match(argv[5], employeeNameDashMatcher)) {
+        //Now we have a valid employee flag
+        //Now check the employee name
+        if(regex_match( argv[6], tokenMatcher)) {
+            //Now sanitized and safe, now put in the map
+            resultingMap["E"] = string(argv[4]);
+        } else {
+            // Employee name was not valid
+            cout << "Employee name was not valid!" << endl;
+            cout << "Employee Name: |" << argv[6] << "|" << endl;
+            return false;
+        }
+    } else if (regex_match(argv[5], guestNameDashMatcher)) {
+        //Now we have a valid guest flag
+        //Now check the guest name
+        if(regex_match( argv[6], tokenMatcher)) {
+            //Now sanitized and safe, now put in the map
+            resultingMap["G"] = string(argv[4]);
+        } else {
+            // Guest name was not valid
+            cout << "Guest name was not valid!" << endl;
+            cout << "Guest Name: |" << argv[6] << "|" << endl;
+            return false;
+        }
+    } else {
+        cout << "Guest OR Employee flag was not present!" << endl;
+        //Token flag was not found
+        return false;
+    }
+
+    //Arrival or leave
+
+    
+    //Room ID (OPTIONAL ARGUMENT)
+    if (argc == 11) {
+        //Then there is an actual room id
+    }
+  
+
+    //Logfile name
+
+    
+   
 
 
+    return true;
 }
 
 
-bool validCommandLineArgumentFlags(char**) {
-    //Check for two situations
-    return false;
-}
-
-void batchRunner(char*) {
-    //Batch file runner
-    cout << "In Batch runner" << endl;
-}
 
 int main(int argc, char* argv[]) {
-    sanatize(argc, argv);
+    map<string, string> sanatizedResult;
 
+    //Inital check of how many arguments
+    cout << "Argument count: " << argc << endl;
 
-    //Print out the args, see what splitting it does
-   for (int i = 0; i < argc; i++) {
-	cout << argv[i] << endl;
-   }
+    if (argc == 3) {
+        cout << "Given batch file" << endl;
+    } else if (argc == 9 || argc == 11) {
+        //Have a full line of arguments
+        cout << "Full line of arguments given" << endl;
+        
+        //Sanatize the input
+        bool validSanatized = sanatizeInput(argc, argv, sanatizedResult);
 
-        //Batch runner
-        if (argc == 3) {
-            //Check for the -B
-            cout << "argv[1]: " << argv[1] << endl;
-            cout << "argv[1] == \"-B\": " << (argv[1] == "-B") << endl;
-
-            if (argv[1] == "-B") {
-                batchRunner(argv[2]);
-            } else{
-                //Not valid command line arguments
-                cout << "Invalid" << endl;
-                return 255;
-            }
-        } else if (argc == 10 || argc == 11) {
-           //Not valid command line arguments
-            // cout << "Invalid" << endl;
-            // return 255; 
-
-            //Longer command used
-            //validCommandLineArgumentFlags(argv);
+        //Was it able to match formatting?
+        if (!validSanatized) {
+            //Not correct format, so invalid
+            cout << "invalid" << endl;
+            return 255;
         }
+
+        //Now that is done, now check if the file exists
+
+    } else {
+        //Not enough args or too many (by count)
+        cout << "invalid" << endl;
+        return 255;
+    }
+
+
+    
+
+
     return 0;
 }
