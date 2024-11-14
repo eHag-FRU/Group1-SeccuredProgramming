eb66611e50c6df0d3b2aa447ee7bd7229ee9f0ca
Elliott Hager <ehager2@kent.edu>
2024-11-14 13:54:39 -0500

    Moved 2 changes over

--- include/logappend.hpp
+++ include/logappend.hpp
@@ -8,6 +8,7 @@
 void resultMapToString(std::map<std::string,std::string>& sanatizedResult);
 bool validTimeStamp(std::map<std::string, std::string>& commandLineArguments, std::fstream& logFile);
 bool validArrivalLeave(std::map<std::string, std::string>& commandLineArguments, std::fstream& logFile);
+std::vector<string> splitString(const string& str, char delimiter);
 
 bool commandExecuter(int argc, char* argv[], std::map<std::string, std::string>& sanatizedResult);
 
--- src/logappend.cpp
+++ src/logappend.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
+#include <sstream>
 
 
 
