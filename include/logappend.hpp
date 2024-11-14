8f8777e49b769d6c903c2fa91cbff77e16f87f0c
Elliott Hager <ehager2@kent.edu>
2024-11-14 13:49:41 -0500

    Updated the HPP for merge request

--- include/logappend.hpp
+++ include/logappend.hpp
@@ -1,9 +1,19 @@
 #ifndef LOG_APPEND_HPP
 #define LOG_APPEND_HPP
 
-#include<map>
-#include<string>
-#include<fstream>
+#include <vector>
+using std::string; using std::map; using std::fstream;
+
+bool sanatizeTime(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool sanatizeToken(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool sanatizeEmployeeOrGuestName(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool sanatizeAriveLeaveTag(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool sanatizeRoomID(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool sanatizeFilePath(int argc, char* argv[], std::map<std::string,std::string>& result);
+bool validArrivalLeave(map<string, string>& commandLineArguments, fstream& logFile, string name);
+std::vector<string> splitString(const string& str, char delimiter);
+void resultMapToString(map<string,string>& sanatizedResult);
+bool sanatizeInput(int argc, char* argv[], std::map<std::string, std::string>& resultingMap);
 
 void resultMapToString(std::map<std::string,std::string>& sanatizedResult);
 bool validTimeStamp(std::map<std::string, std::string>& commandLineArguments, std::fstream& logFile);
