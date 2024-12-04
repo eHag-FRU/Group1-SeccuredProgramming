e5a89c62f4071fb2c4b681624471668b4eadeb4e
Elliott Hager <83028849+eHag-FRU@users.noreply.github.com>
2024-12-03 22:40:25 -0500

    Update encryptionHandler.cpp
    Added in comments to make it easier to follow the encryption and decryption functions

--- src/encryptionHandler.cpp
+++ src/encryptionHandler.cpp
@@ -54,11 +54,20 @@ std::string encrypt(const std::string& message, const std::string& key, map<stri
     //input["-K"] = newKey;
 
     using namespace CryptoPP;
+    //Makes the initilization vector
     byte iv[AES::BLOCKSIZE];
     memset(iv, 0x00, AES::BLOCKSIZE); // Initialize IV to all zeros
+
+    //Will Hold the encrypted message
     std::string encryptedMessage;
+    
     try{
+        //Makes an encryption object based on AES in CBC mode
         CBC_Mode<AES>::Encryption encryption;
+
+        //Sets the encryption key (one added with the padding) with the initilization iv
+        //Uses a string source with the unencrypted text (message), a String Sink for output
+        // after the encryption with a padding scheme set of ZEROS_PADDING
         encryption.SetKeyWithIV((const byte*)newKey.data(), newKey.size(), iv);
             StringSource(message, true,
             new StreamTransformationFilter(encryption,
@@ -68,10 +77,13 @@ std::string encrypt(const std::string& message, const std::string& key, map<stri
     }
     catch (const Exception& ex)
     {
+        //Handles any issues with the encryption
         // Handle exception
         std::cerr << "ENCRYPTION ERROR: " << ex.GetWhat() << endl;
         return "";
     }
+
+    //Returns the successfully encrypted message
     return encryptedMessage;
 }
 
@@ -90,13 +102,25 @@ std::string decrypt(const std::string& encryptedMessage, const std::string& key,
     // cout << "key size: " << key.size() << endl;
 
     using namespace CryptoPP;
+
+     //Makes the initilization vector
     byte iv[AES::BLOCKSIZE];
     memset(iv, 0x00, AES::BLOCKSIZE); // Initialize IV to all zeros
+
+    //Will hold the decrypted message
     std::string decryptedMessage;
+    
     try
     {
+    //Makes an decryption object set for AES CBC decryption mode
     CBC_Mode<AES>::Decryption decryption;
+
+    //Sets the decrption key (padded if needed) with the initilization vector
     decryption.SetKeyWithIV((const byte*)newKey.data(), newKey.size(), iv);
+    //Sets the incoming source of the encrypted source as a string (this is what is passed as an argument)
+    // uses a StreamTransformation filter to pull in the string, decrypt the message
+    //Then the decrypted message is sent out to a StringSink to the decryptedMessage variable
+    //Uses the ZEROS_PADDING block padding scheme to decrypt the message
     StringSource(encryptedMessage, true,
     new StreamTransformationFilter(decryption,
     new StringSink(decryptedMessage), CryptoPP::BlockPaddingSchemeDef::ZEROS_PADDING)
@@ -104,6 +128,7 @@ std::string decrypt(const std::string& encryptedMessage, const std::string& key,
     }
     catch (const Exception& ex)
     {
+        //If any issues with the encryption occur, they are dealt with here
         // Handle exception
         std::cerr << "DECRYPTION ERROR: " << ex.GetWhat() << endl;
         return "";
@@ -111,5 +136,7 @@ std::string decrypt(const std::string& encryptedMessage, const std::string& key,
     }
 
     //cout << "=====END DECRYPT=====" << endl;
+
+    //Returns the successfully decrypted message
     return decryptedMessage;
 }
